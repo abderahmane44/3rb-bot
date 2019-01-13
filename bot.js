a2c17ad3f4b0ac558a15cc790624f2b859a190ac
jamalaldani2 <45242500+jamalaldani2@users.noreply.github.com>
2019-01-13 16:38:45 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = '$'
+const fs = require("fs")
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
